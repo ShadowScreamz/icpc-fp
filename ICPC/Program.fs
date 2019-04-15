ad8f5ba230ebf7f5d48b400fe30e89bce9b5c668
Tshegofatso <tmolobe15@gmail.com>
2019-04-15 21:55:29 +0200

    rivers

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -1,11 +1,23 @@
 ﻿module ICPC
 open System
 
-let commaSprinkler input =
-    failwith "Not implemented"
+let commaSprinkler input = failwith "Not implemented"
 
-let rivers input =
-    failwith "Not implemented"
+let rivers (input: string) =
+    let result = Array.toList (input.Split ' ')
+    let arrlen = result.Length
+    let tester a = 
+        match a = "" with
+        |true -> true
+        |_ ->false
+    match  List.exists tester result    || arrlen <= 2 || input.Contains ',' || input.Contains '!' || input.StartsWith ' ' || input.EndsWith ' '  with // if theres only 1 word or no words or contains punctaution
+    |true -> None
+    |false ->  Some input
+
+ 
+            
+
+
 
 [<EntryPoint>]
 let main argv =
